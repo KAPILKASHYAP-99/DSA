d5d187caf15695442abd006940492ba0dae981ce
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2025-12-08 15:42:04 +0530

    DSA

---
+++ chapter - 8 linklist/pt4.py
@@ -0,0 +1,26 @@
+# topic Insert at the beginning of linked list in  python
+
+class Node:
+    def __init__(self,key):
+        self.key=key
+        self.next=None
+
+def insertBegin(head,key):
+        temp=Node(key)
+        temp.next=head
+        return temp
+
+
+head=None
+head=insertBegin(head,10)
+head=insertBegin(head,20)
+head=insertBegin(head,30)
+
+def printList(head):
+    curr = head
+    while curr!= None:
+        print(curr.key, end=" ")
+        curr = curr.next
+
+
+printList(head)
