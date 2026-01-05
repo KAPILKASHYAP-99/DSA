cee1a171a72133b563eb36294268c8af4c15588f
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2026-01-05 19:52:58 +0530

    DSA

---
+++ chapter-11 Stack/pt1.py
@@ -0,0 +1,14 @@
+# topic - Stack Data structure
+
+from _collections import deque
+stack =deque()
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print(stack)
+print(stack.pop())
+
+top=stack[-1]
+print(top)
+size=len(stack)
+print(size)
