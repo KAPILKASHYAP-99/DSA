2735947a6daa9ccdb5dee2aaa6bca3d196d61b52
KAPILKASHYAP-99 <kapilkashyap.inwork@gmail.com>
2026-02-12 15:25:15 +0530

    dsa

---
+++ chapter-13 Deque/pt1.py
@@ -0,0 +1,45 @@
+# topic - deque in python
+
+# problem 1 using deque
+from _collections import deque
+d=deque()
+d.append(10)
+d.append(20)
+d.append(30)
+d.appendleft(40)
+print(d)
+print(d.pop())
+print(d.popleft())
+print(d)
+
+# problem 2 using a list
+from _collections import deque
+d=deque([10,20,30,40])
+d.insert(2,10)
+print(d.count(10))
+d.remove(10)
+print(d)
+d.extend([50,60])
+print(d)
+d.extend([15,25])
+print(d)
+
+# problem using list (doing rotation)
+from _collections import deque
+d=deque([10,20,30,40,50])
+d.rotate(2)
+print(d)
+d.rotate(-2)
+print(d)
+d.reverse()
+print(d)
+
+# problem 4 (index, access the index)
+from _collections import deque
+d=deque([10,20,30,40,50])
+print(d[2])
+d[2]=100
+print(d)
+print(d[0])
+print(d[-1])
+
